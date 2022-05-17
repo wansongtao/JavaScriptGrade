f25b4d2587a87e26e5730dcd1bd02083383a8cfc
wansongtao <18101837209@163.com>
2022-05-17 12:34:16 +0800

    feat:达夫设备技术实现

--- chapter28/duffDevice.js
+++ chapter28/duffDevice.js
@@ -14,17 +14,19 @@ const getRandomArray = (length) => {
 
 	const arr = [];
 
-  // 使用达夫设备技术，拆成多个循环，大大减少循环次数
+	// 使用达夫设备技术，拆成多个循环，大大减少循环次数
 	let iterations = Math.floor(length / 8);
 	let leftover = length % 8;
 	let i = 0;
 
-  // 余数次
-	do {
-		arr[i++] = Math.floor(Math.random() * 100);
-	} while (--leftover > 0);
+	if (leftover > 0) {
+		// 余数次
+		do {
+			arr[i++] = Math.floor(Math.random() * 100);
+		} while (--leftover > 0);
+	}
 
-  // 倍数次
+	// 倍数次
 	do {
 		arr[i++] = Math.floor(Math.random() * 100);
 		arr[i++] = Math.floor(Math.random() * 100);
@@ -36,9 +38,8 @@ const getRandomArray = (length) => {
 		arr[i++] = Math.floor(Math.random() * 100);
 	} while (--iterations > 0);
 
-  return arr;
+	return arr;
 };
-
 
 const result = getRandomArray(100);
 console.log(result.length);
